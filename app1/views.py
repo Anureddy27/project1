f18983b97eeaca7025c301de77af051b0d74beff
Anureddy27 <bhumireddyanusha143@gmail.com>
2023-11-08 18:58:31 +0530

    main

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Anusha(request):
+    return HttpResponse("<h1><marquee>Anu is a gd girl <h1><marqee>")
